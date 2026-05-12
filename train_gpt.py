134404b2752fadf6eebac9b355220cc1ece6bcd3
Marko Sisovic <msisovic13@gmail.com>
2026-05-12 22:31:54 +0000

    Pin kernels dependency for flash attention

--- train_gpt.py
+++ train_gpt.py
@@ -1625,7 +1625,7 @@ class Hyperparameters:
     # batch sizes
     val_batch_size: int = 4 * 64 * 1024 * 8
     # schedule
-    num_scheduled_iterations: int = 1410  # number of steps to complete lr and ws schedule
+    num_scheduled_iterations: int = 1415  # number of steps to complete lr and ws schedule
     num_extension_iterations: int = 40  # number of steps to continue training at final lr and ws
     # evaluation and logging
     run_id: str = f"{uuid.uuid4()}"
